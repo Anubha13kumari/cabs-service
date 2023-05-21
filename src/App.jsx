1626c19b4b8a3790979ca01ed199ae713f07f9b8
AdarshCoder397 <adarsh397k@gmail.com>
2023-05-21 15:03:22 +0530

    Header added not responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,28 @@
-import './App.css'
-import logo from './assets/vintage.png'
+import "./App.css";
+import logo from "./assets/vintage.png";
+import bg from "./assets/bg.jpg";
 
 function App() {
   return (
-    <div className='app'>
+    <div className="app">
       <div className="navbar">
         <img src={logo} alt="" />
         <button>I'm a driver</button>
       </div>
+      <div className="head">
+        <img className="bg" src={bg} alt="" />
+        <div className="ride">
+          <h1>Get on a ride in just a few clicks!</h1>
+          <h2>Pick-Up Location</h2>
+          <input type="text" />
+          <h2>Drop Location</h2>
+          <input type="text" />
+          <button>Book Now</button>
+
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
