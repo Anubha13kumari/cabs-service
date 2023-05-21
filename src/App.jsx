ec471d4acc601cdd2709c2f056fbb6e2c0381a33
AdarshCoder397 <adarsh397k@gmail.com>
2023-05-21 13:48:13 +0530

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import logo from './assets/vintage.png'
 
 function App() {
   return (
-    <div>
-
+    <div className='app'>
+      <div className="navbar">
+        <img src={logo} alt="" />
+        <button>I'm a driver</button>
+      </div>
     </div>
   )
 }
