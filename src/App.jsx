a7b4346f7e66ab323d707895941179226acac2cf
AdarshCoder397 <adarsh397k@gmail.com>
2023-05-23 14:57:25 +0530

    Cab Selection page with responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,6 @@ function App() {
     <div className="app">
       <Navbar />
       <Header />
-
     </div>
   );
 }
--- src/components/CabSelect/CabSelect.jsx
+++ src/components/CabSelect/CabSelect.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CabSelect.css";
 
-import bg from "../../assets/bg.jpg";
+import bg1 from "../../assets/bg1.webp";
+import car from "../../assets/360-spin.webp";
 
 const CabSelect = ({ pickUp, destination }) => {
+  const [seats, setSeats] = useState(4);
+  const checkSeats = (e) => {
+    setSeats(e.target.value)
+  };
   return (
     <div className="cabSelect">
       <div className="cabSelect__container">
@@ -24,8 +29,39 @@ const CabSelect = ({ pickUp, destination }) => {
             <h3>{destination}</h3>
           </div>
         </div>
+        <div className="cab_details">
+          <h2>
+            Distance : <span>1.7km</span>
+          </h2>
+          <h2>
+            Price: <span>₹67</span>
+          </h2>
+          <h2>
+            Time: <span>22min</span>
+          </h2>
+          <button>Confirm ride</button>
+        </div>
+        <div className="cab_footer">
+          <img src={car} alt="" />
+          <div className="cab_footer_info">
+            <h3>Vintage Alto</h3>
+            <h5>BR-06-BH-9971</h5>
+          </div>
+          <div className="seats">
+            <h3>{`Seats : ${seats}`}</h3>
+            <input
+              type="range"
+              name=""
+              id=""
+              max={6}
+              min={1}
+              value={seats}
+              onChange={(e) => checkSeats(e)}
+            />
+          </div>
+        </div>
       </div>
-      <img className="bgCab" src={bg} alt="" />
+      <img className="bgCab" src={bg1} alt="" />
     </div>
   );
 };
